605af0e62704403288ca82709061d2b5b1fc51b0
이원준 <ggoong8@gmail.com>
2022-06-16 19:45:14 +0900

    security config 수정

--- src/main/java/com/exam/jwt/config/SecurityConfig.java
+++ src/main/java/com/exam/jwt/config/SecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.CorsFilter;
 
 import com.exam.jwt.config.jwt.JwtAuthenticationFilter;
 
@@ -17,22 +15,18 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig{
+	private final CorsConfig corsConfig;
 
-	private final CorsFilter corsFilter;
-	private AuthenticationManager authenticationManager; 
-	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		authenticationManager = http.getSharedObject(AuthenticationManager.class); // https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
-		
-		http.csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않는다.
+		http.csrf().disable().
+		sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않는다.
 		.and()
-		.addFilter(corsFilter) // @CrossOrigin (인증 x), 필터에 등록 (인증 O)
 		.formLogin().disable()
 		.httpBasic().disable() //  헤더의 Authorization 필드에 id, pw를 사용하지 않음 ---> token을 사용하는 방식 : Bearer
-		.addFilter(new JwtAuthenticationFilter( authenticationManager )) // AuthenticationManager
-		.authorizeRequests()
+		.apply(new MyCustomDsl()) // 커스텀 필터 사용
+		.and()
+		.authorizeRequests((authz)-> authz
 		.antMatchers("/api/v1/user/**")
 			.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN') ")
 		.antMatchers("/api/v1/manager/**")
@@ -40,11 +34,18 @@ public class SecurityConfig{
 		.antMatchers("/api/v1/admin/**")
 			.access("hasRole('ROLE_ADMIN') ")
 		.anyRequest()
-			.permitAll();
+			.permitAll());
 		
 		return http.build();
     }
 	
-	
-	
+	public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+			http
+					.addFilter(corsConfig.corsFilter())  // 필터 등록
+					.addFilter(new JwtAuthenticationFilter(authenticationManager));
+		}
+	}
 }
